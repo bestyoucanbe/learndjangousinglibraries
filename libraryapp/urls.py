b7eff4d80f4cdfc91edd3c96e5161544140f8e92
Joy Ittycheriah <bestyoucanbe@yahoo.com>
2019-09-23 09:30:54 -0500

    Added login functionality--able to use http://localhost:8000/accounts/login

--- libraryapp/urls.py
+++ libraryapp/urls.py
@@ -1,9 +1,11 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
 from .views import *
 
 app_name = "libraryapp"
 
 urlpatterns = [
+    url(r'accounts/', include('django.contrib.auth.urls')),
+    url(r'^logout/$', logout_user, name='logout'),
     url(r'^$', home, name='home'),
     url(r'^books$', book_list, name='books'),
     url(r'^librarians$', list_librarians, name='librarians'),
