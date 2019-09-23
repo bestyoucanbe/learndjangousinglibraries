05a225b2a04826ff5f1306134f1fcda61d227de7
Joy Ittycheriah <bestyoucanbe@yahoo.com>
2019-09-23 06:16:32 -0500

    added a message to the home page

--- libraryapp/urls.py
+++ libraryapp/urls.py
@@ -4,7 +4,7 @@ from .views import *
 app_name = "libraryapp"
 
 urlpatterns = [
-    url(r'^$', book_list, name='home'),
+    url(r'^$', home, name='home'),
     url(r'^books$', book_list, name='books'),
     url(r'^librarians$', list_librarians, name='librarians'),
     url(r'^libraries$', list_libraries, name='libraries')
