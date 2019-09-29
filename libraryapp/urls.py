95e5ac70fea4ace3a41c082389368b88b5393634
Joy Ittycheriah <bestyoucanbe@yahoo.com>
2019-09-29 15:58:36 -0500

    able to add books to libraries--post request

--- libraryapp/urls.py
+++ libraryapp/urls.py
@@ -9,5 +9,6 @@ urlpatterns = [
     url(r'^$', home, name='home'),
     url(r'^books$', book_list, name='books'),
     url(r'^librarians$', list_librarians, name='librarians'),
-    url(r'^libraries$', list_libraries, name='libraries')
+    url(r'^libraries$', list_libraries, name='libraries'),
+    url(r'^book/form$', book_form, name='book_form')
 ]
